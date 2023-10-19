3973ccbd4df37b3dfc6016660ab528f849ecbdc9
samual <taylosa5@oregonstate.edu>
2023-10-19 14:01:05 -0700

    Added stuff

--- 14 - Final Words.py
+++ 14 - Final Words.py
@@ -1,2 +1,22 @@
 # Congratulations!
-# You know have all of the tools you need to solve the first LeetCode assignment.
+# You know have all of the tools you need to solve the first LeetCode assignme
+# getUserInput(letter A, letter B, letter C, letter D, letter E)
+# 5LetterWord = getWordFrom5LetterWordDictionary(random from 0 to i-1)
+# SplitWordInto5Letters(A, B, C, D, E)
+# For i in list 5LetterWord {
+# If Letter A == AnyOfTheLettersInTheChosenWord {
+#     Letter A.color == yellow
+#     If Letter A == 5LetterWordLetterA {
+#         Letter A.color == green
+#     }
+# }
+# Print(Letter A (color))
+# repeat above for all 5 letters in user input
+# }
+# If getUserInput == 5LetterWord {
+#     Print("You Win")
+# }
+# Else {
+#     Print("Guess again")
+#     restartGame()
+# }
